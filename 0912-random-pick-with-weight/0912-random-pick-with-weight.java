3cd3d4aceadedf30eed3136b0a54dcb12bdefb91
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-01 19:37:47 +0530

    Time: 81 ms (12.69%), Space: 49.8 MB (50.61%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.java
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.java
@@ -1,26 +1,24 @@
-
-
-import static java.lang.Math.random;
-
 class Solution {
-    int totalSum =0;
     int[] prefixSum;
+    int totalSum=0;
     Random rnd;
+
     public Solution(int[] w) {
-        prefixSum = new int [w.length];
-        for(int i=0;i<w.length; i++){
-            totalSum += w[i]; 
+        prefixSum = new int[w.length];
+        for(int i=0; i< w.length; i++){
+            totalSum += w[i];
             prefixSum[i] = totalSum;
-        }
-        rnd = new Random(totalSum);
+        }  
+        rnd = new Random();  
     }
     
     public int pickIndex() {
-        int target = new Random().nextInt(totalSum);
+        int target = rnd.nextInt(totalSum) ;
 
-        for(int i=0; i<prefixSum.length; i++){
-            if(target < prefixSum[i]) return i;
+        for(int i=0; i< prefixSum.length; i++){
+            if (target < prefixSum[i]) return i;
         }
+
         return -1;
     }
 }
