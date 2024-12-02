92e747809c60ea1c8b02ac954eb145b4f54c336b
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-02 14:56:14 +0530

    Time: 13 ms (12.88%), Space: 59.2 MB (6.49%) - LeetHub

--- 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.java
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.java
@@ -1,39 +1,26 @@
 class SparseVector {
-    //Map<Integer, Integer> inducesWithOne = new HashMap();
-
-    List<int[]> pairs;
-
+    List<Pair<Integer,Integer>> pairs;
+    
     SparseVector(int[] nums) {
         pairs = new ArrayList<>();
-
-        for(int i=0; i< nums.length; i++){
-            if(nums[i] != 0)
-                pairs.add(new int[]{i,nums[i]});
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) pairs.add(new Pair(i, nums[i]));
         }
+
+        
     }
     
 	// Return the dotProduct of two sparse vectors
     public int dotProduct(SparseVector vec) {
-        int sum=0;
-        
-        //Approach 1) Using HashMap - Someitmes Interviewer will not accept and will ask for another solution.
-        /*if (inducesWithOne == null) return sum;
-        Set<Integer> ks = inducesWithOne.keySet();
-        ks.retainAll(vec.inducesWithOne.keySet());
-        for (Integer idx : ks) {
-            sum += inducesWithOne.get(idx)* vec.inducesWithOne.get(idx);
-        }
-        */
-       int p =0;
-       int q=0;
+        int result =0;
 
-       while (p < this.pairs.size() && q < vec.pairs.size()) {
-
-            int thisIdx = this.pairs.get(p)[0];
-            int vecIdx = vec.pairs.get(q)[0];
+        int p=0, q=0;
+        while (p < this.pairs.size() && q < vec.pairs.size()) {
+           int thisIdx = this.pairs.get(p).getKey();
+           int vecIdx =vec.pairs.get(q).getKey();
 
             if (thisIdx == vecIdx) {
-                sum += this.pairs.get(p)[1] * vec.pairs.get(q)[1];
+                result += (this.pairs.get(p).getValue() * vec.pairs.get(q).getValue());
 
                 p++;
                 q++;
@@ -42,9 +29,8 @@ class SparseVector {
                 if (thisIdx < vecIdx) p++;
                 else q++;
             }
-       }
-
-        return sum;
+        }
+        return result;
     }
 }
 
