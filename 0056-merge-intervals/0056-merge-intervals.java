6819dfabc62a3797b4de0dd684e058f20605e7f3
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-02 17:45:52 +0530

    Time: 10 ms (25.14%), Space: 46.3 MB (72.98%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        LinkedList<int[]> results = new LinkedList<int[]>();
+        LinkedList<int[]> results = new LinkedList<>();
 
         Arrays.sort(intervals, (a1, a2) -> Integer.compare(a1[0], a2[0]));
 
         for (int[] iv : intervals) {
-            if(results.isEmpty()){
-                results.add(iv);
-                continue;
+            if(results.isEmpty()) results.add(iv);
+            else {
+
+                int[] lastEle = results.getLast();
+
+                if(lastEle[1] > iv[0] || iv[0] <= lastEle[1]){
+                    lastEle[0] = Math.min(iv[0], lastEle[0]);
+                    lastEle[1] = Math.max(iv[1], lastEle[1]);
+                }
+                else
+                    results.add(iv);                            
             }
-            Boolean isOverlap = false;
-            if(results.getLast()[1] >= iv[0] || iv[0] < results.getLast()[1])
-                isOverlap = true;
-            
-            if(isOverlap){
-                results.getLast()[0] = Math.min(iv[0], results.getLast()[0]);
-                results.getLast()[1] = Math.max(iv[1], results.getLast()[1]);
-            }
-            else
-                results.add(iv);
         }
+
+
         return results.toArray(new int[results.size()][2]);
     }
 }
