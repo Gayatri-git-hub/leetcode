e73e65241f4c19d68ea9b112d5cf65cf2ef6ac03
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-02 17:37:06 +0530

    Time: 9 ms (51.24%), Space: 46 MB (97.9%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,35 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> results = new ArrayList<>();
+        LinkedList<int[]> results = new LinkedList<int[]>();
 
-        Comparator<int[]> c = new Comparator<int[]>() {
-            public int compare(int[] a1, int[] a2){
-                return a1[0] - a2[0];
+        Arrays.sort(intervals, (a1, a2) -> Integer.compare(a1[0], a2[0]));
+
+        for (int[] iv : intervals) {
+            if(results.isEmpty()){
+                results.add(iv);
+                continue;
             }
-        };
-
-        Arrays.sort(intervals, c);
-
-        int[] currInterval = new int[]{intervals[0][0], intervals[0][1]};
-
-        for (int i = 1; i < intervals.length; i++) {
-            if(currInterval[1] >= intervals[i][0]){
-                
-                currInterval = new int[]{Math.min(currInterval[0],intervals[i][0]) , Math.max(currInterval[1], intervals[i][1])};
+            Boolean isOverlap = false;
+            if(results.getLast()[1] >= iv[0] || iv[0] < results.getLast()[1])
+                isOverlap = true;
+            
+            if(isOverlap){
+                results.getLast()[0] = Math.min(iv[0], results.getLast()[0]);
+                results.getLast()[1] = Math.max(iv[1], results.getLast()[1]);
             }
-            else {
-                results.add(currInterval);
-                currInterval = new int[]{intervals[i][0], intervals[i][1]};
-            }
+            else
+                results.add(iv);
         }
-
-        results.add(currInterval);
-
-        int[][] ints = new int[results.size()][2];
-        for(int i=0; i<results.size(); i++){
-            ints[i] = results.get(i);
-        }
-        
-        return ints;
+        return results.toArray(new int[results.size()][2]);
     }
 }
