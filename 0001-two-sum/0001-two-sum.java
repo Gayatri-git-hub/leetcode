6426689707dd63eafc217d0e730fbe05618c763e
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-03 11:12:41 +0530

    Time: 142 ms (5.06%), Space: 44.9 MB (50.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,9 +4,12 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (!map.containsKey(nums[i])) map.put(nums[i], i);
+            
+            if(map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i)
+                return new int[]{i, map.get(target - nums[i])};
+                
+            map.put(nums[i], i);
         
-            if(map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i) return new int[]{i, map.get(target - nums[i])};
         }
 
         return new int[]{-1, -1};
