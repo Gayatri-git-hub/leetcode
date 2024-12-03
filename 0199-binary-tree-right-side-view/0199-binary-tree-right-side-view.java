0ff3e84cb915360c5c16edafd7025bef3a5a3756
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-03 13:33:54 +0530

    Time: 1 ms (67.18%), Space: 42.2 MB (32.64%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,7 +14,29 @@
  * }
  */
 class Solution {
+    List<Integer> ans = new LinkedList<>();
+
+    private void dfs(TreeNode node,int level){
+        if(node == null) return;
+
+        if(level == ans.size()) ans.add(node.val);
+
+        dfs(node.right, level + 1);
+        dfs(node.left, level + 1);
+
+    }
     public List<Integer> rightSideView(TreeNode root) {
+
+        //Approach 1) using BFS -Which I feel good
+        //return bfs(root);
+
+        //Approach 2) using DFS 
+        dfs(root, 0);
+
+        return new ArrayList<>(ans);
+    }
+
+    private List<Integer> bfs(TreeNode root){
         Queue<Pair<Integer, TreeNode>> que = new LinkedList<>();
 
         que.offer(new Pair<Integer, TreeNode>(0, root));
