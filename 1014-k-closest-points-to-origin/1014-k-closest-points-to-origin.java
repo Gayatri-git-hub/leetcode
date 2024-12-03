a52ae5951bc4332ea38f95820cd875c07b30d79b
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-03 13:51:31 +0530

    Time: 30 ms (61.01%), Space: 55.7 MB (8.11%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,42 +1,36 @@
-class Point implements Comparable<Point>{
+class Point{
     int x;
     int y;
     int dist;
 
-   public Point(int x1, int y1, int d){
-        this.x = x1;
-        this.y = y1;
-        this.dist= d;
-    }
-
-    public int compareTo(Point y){
-        return Integer.compare(y.dist, this.dist);
+    public Point(int x1, int y1, int d){
+        x= x1;
+        y=y1;
+        dist = d;
     }
 }
 
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        
-        if(points.length == 0 ) return new int[0][0];
+        PriorityQueue<Point> maxHeap = new PriorityQueue<>((p1, p2) -> p2.dist - p1.dist);
 
-        PriorityQueue<Point> maxHeap = new PriorityQueue<Point>();
+        for (int i = 0; i < points.length; i++) {
+            int x = points[i][0];
+            int y = points[i][01];
+            int d = x*x + y*y;
 
-        for(int i=0; i< points.length; i++){
-            int dist = (points[i][0] * points[i][0]) + (points[i][1] * points[i][1]);
+            maxHeap.offer(new Point(x, y, d));
 
-            Point p = new Point(points[i][0], points[i][1], dist);
-
-            maxHeap.add(p);
-            if(maxHeap.size() > k)
-                maxHeap.remove();            
+            if (maxHeap.size() > k) maxHeap.poll();
         }
-        int[][] res = new int[k][2];
+        int[][] ans = new int[maxHeap.size()][2]; 
 
-        for(int i= 0; i<k; i++) {
-            Point p= maxHeap.poll();
-            res[i] = new int[]{p.x, p.y};
+        for (int i = 0; i < k; i++) {
+           Point p =maxHeap.poll();
+           ans[i][0] = p.x;
+           ans[i][1] = p.y;
         }
 
-        return res;
+        return ans;       
     }
 }
