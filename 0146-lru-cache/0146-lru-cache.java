7bb092226da1eea423e28567eba58e70d3d6a840
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-03 16:44:38 +0530

    Time: 43 ms (85.16%), Space: 111.7 MB (86.12%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,74 +1,25 @@
-class ListNode{
-    int key;
-    int value;
-    ListNode prev;
-    ListNode next;
-
-    public ListNode(int k, int v){
-        key = k;
-        value = v;
-    }
-}
-
 class LRUCache {
-    HashMap<Integer, ListNode> map;
-    ListNode head;
-    ListNode tail;
+    LinkedHashMap<Integer, Integer> cache;
     int cacheCapacity;
 
     public LRUCache(int capacity) {
         this.cacheCapacity = capacity;
-        map = new HashMap<>();
-        
-        head = new ListNode(-1,-1);
-        tail =new ListNode(-1,-1);
-
-        head.next= tail;
-        tail.prev= head;
+        cache = new LinkedHashMap<>(capacity, 100.00f, true){
+            @Override
+            public boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest){
+                return size() > cacheCapacity;
+            }
+        };
     }
     
     public int get(int key) {
-        if (map.containsKey(key)) {
-            ListNode n = map.get(key);
-
-            remove(n);
-            add(n);
-
-            return n.value;
-        }
-        else
-            return -1;
+        if (cache.containsKey(key)) return cache.get(key);
+        else return -1;
         
     }
     
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            remove(map.get(key));
-        }
-        
-        ListNode n = new ListNode(key,value);
-        map.put(key, n);
-        add(n);
-
-        if (map.size() > cacheCapacity) {
-            ListNode lruNode = head.next;
-            remove(lruNode);
-            map.remove(lruNode.key);
-        }
-    }
-
-    public void add(ListNode node){
-        ListNode prevEnd = tail.prev;
-        prevEnd.next = node;
-
-        node.next = tail;
-        tail.prev =node;
-        node.prev = prevEnd;
-    }
-
-    public void remove(ListNode node){
-       node.prev.next = node.next;
-       node.next.prev = node.prev;
+        cache.put(key, value);
     }
 }
 
