7ade887126782a2a26780b0db6013bf27fb5b4db
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-04 18:59:12 +0530

    Time: 6 ms (8.43%), Space: 45.7 MB (36.43%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.java
+++ 1028-interval-list-intersections/1028-interval-list-intersections.java
@@ -9,41 +9,40 @@ class Interval{
 
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        
-        List<Interval> res = new ArrayList<>();
 
         int i=0;
         int j=0;
-        
+        List<Interval> res =new ArrayList<>();
+
         while (i < firstList.length && j < secondList.length) {
             Interval first = new Interval(firstList[i][0], firstList[i][1]);
             Interval second = new Interval(secondList[j][0], secondList[j][1]);
 
-            if(first.start > second.end) {
+            if (first.start > second.end ) {
                 j++;
                 continue;
             }
 
-            if(second.start > first.end) {
+            if(second.start > first.end){
                 i++;
                 continue;
             }
 
-            Interval intersect = new Interval(Math.max(first.start, second.start),
-             Math.min(first.end, second.end));
+            int s = Math.max(first.start, second.start);
+            int e =Math.min(first.end, second.end);
+            Interval intersect = new Interval(s,e);
 
             res.add(intersect);
 
             if (first.end > second.end) j++;
             else i++;
-        }
-
+            
+        }   
         int[][] ans = new int[res.size()][2];
-        for(int a=0; a< res.size(); a++){
-            ans[a] = new int[]{res.get(a).start, res.get(a).end};
-        }
+        for (int k = 0; k < res.size(); k++) {
+            ans[k] = new int[] {res.get(k).start, res.get(k).end};
+        } 
 
         return ans;
-
     }
 }
