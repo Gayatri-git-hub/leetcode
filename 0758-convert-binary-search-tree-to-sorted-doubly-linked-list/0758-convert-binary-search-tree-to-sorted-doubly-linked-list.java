d2e46db9c31620669269f57e967bf84a658836de
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-04 11:49:42 +0530

    Time: 0 ms (100%), Space: 42.6 MB (5.34%) - LeetHub

---
+++ 0758-convert-binary-search-tree-to-sorted-doubly-linked-list/0758-convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+    Node first;
+    Node last;
+
+    public Node treeToDoublyList(Node root) {
+
+        if (root == null) return null;
+
+        inOrder(root);
+        
+        first.left = last;
+        last.right = first;
+
+        return first;
+    }
+
+    private void inOrder(Node node){
+        if (node != null) {
+            inOrder(node.left);
+            
+            if (last == null)
+                first = node;
+            else{
+                node.left = last;
+                last.right = node;
+            }
+
+            last = node;
+
+            inOrder(node.right);
+        }
+
+    }
+}
