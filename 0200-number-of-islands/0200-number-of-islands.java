094a5dcdcc1b93fa54c4b63236af5cd4ff4a132b
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-04 12:37:20 +0530

    Time: 5 ms (33.62%), Space: 49.6 MB (41.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,35 +1,33 @@
 class Solution {
     int m;
     int n;
-    int[][] directions = new int[][]{{-1,0},{1,0}, {0, -1}, {0, 1}};
+    int[][] dirs = new int[][]{{-1,0}, {1,0}, {0, -1}, {0, 1}};
 
     public int numIslands(char[][] grid) {
-        int numOfIslands =0;
-        
-        m = grid.length;
-        n = grid[0].length;
+        int numOfIslands=0;
+        m= grid.length;
+        n =grid[0].length;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') numOfIslands += 1;
-
-                setSurroundingsZero(grid, i, j);
-
-            }
-        }
-
+           for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == '1') 
+                        numOfIslands += 1;
+                    
+                    setSurroundingsToZero(grid, i, j);
+                } 
+           } 
         return numOfIslands;
     }
 
-    private void setSurroundingsZero(char[][] grid, int i, int j){
-        if(0 <= i && i <m && 0<= j && j < n && grid[i][j] == '1') 
-            grid[i][j] ='0';
+    private void setSurroundingsToZero(char[][] grid, int i, int j){
+        if(0 <= i && i < m && 0 <= j && j < n && grid[i][j] == '1')
+            grid[i][j] = '0';
         else
             return;
-            
-        for (int[] dir : directions) {
-            setSurroundingsZero(grid, i + dir[0], j + dir[1]);
-        }
+        
+        for (int[] d : dirs) 
+            setSurroundingsToZero(grid, i + d[0], j + d[1]);
+        
         return;
     }
 }
