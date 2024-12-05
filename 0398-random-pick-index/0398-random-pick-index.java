39bf014cda99a1a5a865115d5858a620e8e300fb
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-05 15:31:45 +0530

    Time: 71 ms (97.53%), Space: 55.6 MB (72.61%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.java
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -1,4 +1,23 @@
+//With Hashmap
 class Solution {
+    Map<Integer, List<Integer>> map;
+    Random rnd = new Random();
+    public Solution(int[] nums) {
+        map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+    }
+    
+    public int pick(int target) {
+        List<Integer> indices = map.get(target);
+        return indices.get(rnd.nextInt(indices.size()));
+    }
+}
+
+//Without Hashmap
+/*class Solution {
     int[] numbs;
     Random rnd;
     public Solution(int[] nums) {
@@ -21,6 +40,8 @@ class Solution {
         return result;
     }
 }
+*/
+
 
 /**
  * Your Solution object will be instantiated and called as such:
