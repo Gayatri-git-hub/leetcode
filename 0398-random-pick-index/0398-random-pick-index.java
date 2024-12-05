d3964916e457cafa0045ca6266cec8d1cd3b84f0
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-05 15:21:42 +0530

    Time: 1508 ms (17.78%), Space: 55.3 MB (82.1%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.java
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -1,20 +1,20 @@
 class Solution {
-    Random rdm = new Random();
-    int result = -1;
     int[] numbs;
-
+    Random rnd;
     public Solution(int[] nums) {
+        rnd = new Random();
         numbs = nums;
     }
     
     public int pick(int target) {
         int counter=0;
+        int result = -1;
         for (int i = 0; i < numbs.length; i++) {
             if(numbs[i] == target){
                 counter++;
-
-                if(rdm.nextInt(counter) == counter - 1)
-                    result=i;
+            
+                if(rnd.nextInt(counter) == counter -1)
+                    result = i;
             }
         }
 
