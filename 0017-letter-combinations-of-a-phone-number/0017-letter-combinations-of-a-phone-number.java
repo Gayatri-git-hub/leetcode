63ec917a3b1c971e11c3761eb939b5567217181c
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2024-12-05 09:00:54 +0530

    Time: 5 ms (27.3%), Space: 42.5 MB (7.04%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,29 +1,26 @@
 class Solution {
+     Map<Character, String> map = Map.of('2', "abc",'3', "def",'4', "ghi",'5', "jkl",'6', "mno",'7', "pqrs",'8', "tuv", '9', "wxyz");
+
     List<String> res;
-
-    Map<Character, String> map = Map.of('2', "abc",'3', "def",'4', "ghi",'5', "jkl",'6', "mno",'7', "pqrs",'8', "tuv", '9', "wxyz");
-
     public List<String> letterCombinations(String digits) {
-
         res = new LinkedList<>();
-        if(digits.isEmpty()) return res;
-        
+        if(digits.length() == 0) return res;
+
         backtrack(digits, 0, "");
 
         return res;
     }
 
-    private void backtrack(String digits, int index, String currStr){
-        if (index == digits.length()) {
-            res.add(currStr.toString());
+    private void backtrack(String digits, int index, String curr){
+
+        if(index == digits.length()){
+            res.add(curr);
             return;
         }
-        
+
         String letters = map.get(digits.charAt(index));
-
         for (char c : letters.toCharArray()) {
-            backtrack(digits, index + 1, currStr + c);
+            backtrack(digits, index + 1, curr + c);
         }
-
     }
 }
