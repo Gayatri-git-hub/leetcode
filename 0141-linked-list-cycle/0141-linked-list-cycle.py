5a73e56fa3e38d336628d53ca1dfa90421c190a9
Gayatri-git-hub <105269331+Gayatri-git-hub@users.noreply.github.com>
2025-07-21 12:56:02 +0530

    Time: 56 ms (13.21%), Space: 20 MB (17.43%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,7 +6,26 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        # Option 1) Using visited Hash Table
+
+        # Option 2) Better for Space complxity O(1)
+        # Floyd's Cycle detection : Fast & Slow Pointers
+
+        if head is None or head.next is None:
+            return False
+        
+        slow = head
+        fast = head.next
+
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+
+        return True
+
+        # Option 1) Using visited Hash Table 
+        # Time Complexity = O(N) and Space Complexity = O(N)
 
         my_dict = {}
 
